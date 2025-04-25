07ef8521640715e21b614e79dbef9f60d41e74d8
wrightdj99 <wrightdj99@gmail.com>
2025-04-24 21:44:13 -0500

    Testing with new repo name.

--- server/src/middlewares/roleMiddleware.js
+++ server/src/middlewares/roleMiddleware.js
@@ -1,7 +1,7 @@
 const authorizeRoles = (...allowedRoles) => {
     return (req, res, next) => {
         if (!allowedRoles.includes(req.user.role)) {
-            return res.status(403).json({message: "Access denied"});
+            return res.status(403).json({message: "Access denied."});
         }
         next();
     }
