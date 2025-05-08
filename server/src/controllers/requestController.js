983ff3b550a9950bba02e8811552a0c8bc3fa17d
wrightdj99 <wrightdj99@gmail.com>
2025-05-08 15:04:14 -0500

    Request API is working!

--- server/src/controllers/requestController.js
+++ server/src/controllers/requestController.js
@@ -1,7 +1,22 @@
 const Request = require("../models/requestModel");
+const User = require("../models/userModel");
 
 const createRequest = async (req, res) => {
     try {
+        const { username, title, content } = req.body; 
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(404).json({message: `No user was found with that username`});
+        }
+
+        const newRequest = new Request({
+            author: user._id,
+            title,
+            content,
+        });
+
+        await newRequest.save();
+
         res.status(201).json({message: `Successfully created a request`});
     } catch (error) {
         res.status(500).json({message: "An error occurred."});
@@ -10,7 +25,17 @@ const createRequest = async (req, res) => {
 
 const editRequest = async (req, res) => {
     try {
-        res.status(201).json({message: `Successfully edited a request`});
+        const { username, title, content } = req.body;
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(404).json({message: `No user was found with that username`});
+        }
+        Request.updateOne(
+            {author: user._id, title: title}, 
+            {title: title, content: content}
+        ).then(() => {
+            res.status(200).json({message: `A request was updated`});
+        })
     } catch (error) {
         res.status(500).json({message: "An error occurred."});
     }
@@ -18,7 +43,14 @@ const editRequest = async (req, res) => {
 
 const getAllRequests = async (req, res) => {
     try {
-        res.status(201).json({message: `Getting every request`});
+        const { username } = req.body;
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(404).json({message: `No user was found with that username`});
+        }
+        Request.find({ author: user._id }).then(result => {
+            res.status(200).json({data: result});
+        });
     } catch (error) {
         res.status(500).json({message: "An error occurred."});
     }
@@ -26,7 +58,14 @@ const getAllRequests = async (req, res) => {
 /* For admin only */
 const deleteRequest = async (req, res) => {
     try {
-        res.status(201).json({message: `Admin successfully deleted a request`});
+        const { username, title } = req.body;
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(404).json({message: `No user was found with that username`});
+        }
+        Request.findOneAndDelete({ author: user._id, title: title}).then(result => {
+            res.status(201).json({message: "Delete was successful"});
+        });
     } catch (error) {
         res.status(500).json({message: "An error occurred."});
     }
@@ -34,7 +73,21 @@ const deleteRequest = async (req, res) => {
 /* For admin only */
 const assignRequest = async (req, res) => {
     try {
-        res.status(201).json({message: `Admin successfully assigned a request`});
+        const { username, assigneeUsername, title } = req.body;
+        const user = await User.findOne({ username });
+        const assignee = await User.findOne({ username: assigneeUsername, $or: [{role: "manager" }, {role: "admin"}]});
+        if (!user) {
+            return res.status(404).json({message: `No user was found with that username`});
+        }
+        if (!assignee) {
+            return res.status(404).json({message: `No assignee was found with that username`});
+        }
+        Request.updateOne(
+            {author: user._id, title: title}, 
+            {assignee: assignee._id}
+        ).then(() => {
+            res.status(200).json({message: `A request was assigned`});
+        });
     } catch (error) {
         res.status(500).json({message: "An error occurred."});
     }
